3abe878b84c5383b30972330a83de264148ab7bb
yaobw <bowen.yao@ideacome.com>
2020-07-28 13:47:37 +0800

    PlusOne

---
+++ Week_01/PlusOne.java
@@ -0,0 +1,31 @@
+/**
+ * @description:
+ * @author: yaobw
+ * @date: 2020/7/28 1:47 下午
+ */
+public class PlusOne {
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i] += 1;
+                return digits;
+            }
+
+        }
+        //如果所有位都是进位，则长度+1
+        digits= new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = new int[3];
+        digits[0] = 1;
+        digits[1] = 9;
+        digits[2] = 9;
+        plusOne(digits);
+    }
+}
